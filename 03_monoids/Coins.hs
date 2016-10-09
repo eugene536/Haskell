c39cabdfcb1258dc20398330aa91f45fa13ce770
Eugene Nemchenko <n.eugene536@gmail.com>
2016-10-09 22:08:41 +0300

    upd Coins

--- 03_monoids/Coins.hs
+++ 03_monoids/Coins.hs
@@ -1,19 +1,19 @@
-{-# LANGUAGE PolyKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeOperators              #-}
 
 module Coins where
-import Data.Monoid 
-import Data.Typeable
-import Debug.Trace (trace)
+import           Data.Monoid
+import           Data.Typeable
+import           Debug.Trace   (trace)
 
-newtype Coin color = Coin 
-                    { getCoin :: Int } 
+newtype Coin color = Coin
+                    { getCoin :: Int
+                    } deriving (Num, Show)
 data Blue
 data Red
 
@@ -23,22 +23,14 @@ createCoin _ = Coin
 c1 = createCoin (undefined::Blue) 10
 c2 = Coin 5 :: Coin Red
 
-instance Num (Coin a) where
-    (Coin a) + (Coin b) = Coin (a + b)
-    (Coin a) * (Coin b) = Coin (a * b)
-    abs (Coin a)        = Coin (abs a)
-    signum (Coin a)     = Coin (signum a)
-    fromInteger a       = Coin (fromInteger a)
-    negate (Coin a)     = Coin (negate a)
-
 instance Monoid (Coin a) where
-    mempty                    = Coin 0
-    mappend (Coin a) (Coin b) = Coin (a + b)
-
+    mempty  = 0
+    mappend = (+)
 
 getInnerType :: Coin a -> a
 getInnerType _ = undefined
--- compCoins :: (Typeable a, Typeable b) => Coin a -> Coin b -> Ordering
+
+compCoins :: (Typeable a, Typeable b) => Coin a -> Coin b -> Ordering
 compCoins x y = compare l r
     where
         l = show $ typeOf $ getInnerType x
