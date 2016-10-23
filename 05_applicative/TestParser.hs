baa21bcbfb039d0974e700c89240748188c55197
Eugene Nemchenko <n.eugene536@gmail.com>
2016-10-23 23:07:39 +0300

    without last exercise

--- 05_applicative/TestParser.hs
+++ 05_applicative/TestParser.hs
@@ -1,28 +1,11 @@
 module TestParser where
 
 import           Control.Applicative (Alternative (..), liftA2, liftA3)
-import           Data.Char           (isDigit, isUpper)
-import           Parser              (Parser (..))
 import           Control.Monad       (void)
+import           Data.Char           (isUpper)
+import           Parser              (Parser (..))
+import           UsefulParsers       (char, posInt, satisfy)
 
-satisfy :: (Char -> Bool) -> Parser Char
-satisfy p = Parser f
-    where
-        f [] = Nothing
-        f (x:xs)
-            | p x       = Just (x, xs)
-            | otherwise = Nothing
-
-char :: Char -> Parser Char
-char c = satisfy (== c)
-
-posInt :: Parser Integer
-posInt = Parser f
-    where
-        f xs
-            | null ns   = Nothing
-            | otherwise = Just (read ns, rest)
-            where (ns, rest) = span isDigit xs
 
 abParser :: Parser (Char, Char)
 abParser = liftA2 (,) (char 'a') $ char 'b'
